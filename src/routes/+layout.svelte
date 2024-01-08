fb3e6f3f90a0360ae569a08af86424ee8772c054
Michal Černý <michal@cerny.me>
2024-01-08 23:05:05 +0100

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
     import { afterNavigate } from "$app/navigation";
     import "../app.css";
-  
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    
+    inject({ mode: dev ? 'development' : 'production' });
+
     afterNavigate(() => {
       //window.HSStaticMethods.autoInit();
     });
